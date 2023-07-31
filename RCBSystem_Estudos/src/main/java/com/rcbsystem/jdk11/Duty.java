7a7dcc84ce884b8c57118210b7bca5d7b0f1a4a2
RCBSystem <rcbsystem@gmail.com>
2023-07-30 21:54:55 -0300

    Atualização 30/07/2023

---
+++ RCBSystem_Estudos/src/main/java/com/rcbsystem/jdk11/Duty.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rcbsystem.jdk11;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public  class Duty {
+        private String id;
+        private int mainDuty;
+        private String additionalDuties;
+
+    public Duty() {
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the mainDuty
+     */
+    public int getMainDuty() {
+        return mainDuty;
+    }
+
+    /**
+     * @param mainDuty the mainDuty to set
+     */
+    public void setMainDuty(int mainDuty) {
+        this.mainDuty = mainDuty;
+    }
+
+    /**
+     * @return the additionalDuties
+     */
+    public String getAdditionalDuties() {
+        return additionalDuties;
+    }
+
+    /**
+     * @param additionalDuties the additionalDuties to set
+     */
+    public void setAdditionalDuties(String additionalDuties) {
+        this.additionalDuties = additionalDuties;
+    }
+
+        
+      }
